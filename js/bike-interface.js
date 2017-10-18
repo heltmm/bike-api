8e0de7de7b33a73084cd3b7085f53496649e94fb
Mark Helt and karla buckner <mark+karla@>
2017-10-18 15:30:32 -0700

    fix error message not showing

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,11 +1,22 @@
 import {Bike} from './../js/bike.js';
 
 $(document).ready(function() {
-  $('#bikeButton').click(function() {
+  $('#bikeIdButton').click(function() {
     let bikeId = $("#bikeId").val();
-
-    let bike = new Bike()
-    bike.bikeApi(bikeId);
-
+    $('#bikeId').val("");
+    $('#output').html('')
+    let bike = new Bike();
+    bike.bikeById(bikeId);
+  });
+  $('#bikeSearchButton').click(function() {
+    let search = $("#search").val();
+    $('#search').val();
+    $('#patient').html("<h3>Please be Patient this may take a while...</h3>")
+    $('#output').html('')
+    setTimeout(function(){
+      alert("Please Continue to be Patient");
+    }, 5000);
+    let bike = new Bike();
+    bike.bikeBySearch(search);
   });
 });
--- js/bike.js
+++ js/bike.js
@@ -1,24 +1,58 @@
 export class Bike {
-  bikeApi(id) {
+
+  bikeById(id) {
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `https://bikeindex.org:443/api/v3/bikes/${id}`;
+      console.log(url);
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
         } else {
-          reject(Error(request.statusText));
+          reject(Error(request.response));
         }
-      }
+      };
+
       request.open("GET", url, true);
       request.send();
     });
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      $("#output").html(`<ul><li>${body.bike.title}</li><li>${body.bike.serial}</li><ul>`);
+      let keys = Object.keys(body.bike);
+
+      $("#output").html(`<h2>${body.bike.title}</h2>`);
       }, function(error) {
-      return error.message;
+        console.log(error);
+        $("#error").html(`<h2>${error}</h2>`);
+    });
+  }
+  bikeBySearch(search) {
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&query=${search}&location=IP&distance=10&stolenness=stolen`;
+      console.log(url);
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.response));
+        }
+      };
+
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      body.bikes.forEach(function(bike){
+        $("#output").append(`<h2>${bike.title}</h2>`);
+      });
+      $('#patient').html("");
+      }, function(error) {
+        console.log(error);
+        $("#error").html(`<h2>${error.message}</h2>`);
     });
   }
 }
